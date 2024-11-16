eb4f262f05f8b78312eaf59b631a55d4cfbcf106
ZxySynap <134072667+ZxySynap@users.noreply.github.com>
2024-11-16 23:30:12 +0700

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -799,7 +799,7 @@ function HorizonBarLibrary:CreateWindow(WindowProperties)
             Visible = true,
             ClipsDescendants = false,
             FontFace = Font.new("rbxasset://fonts/families/Ubuntu.json", Enum.FontWeight.Bold),
-            Text = os.time,
+            Text = os.time(),
             TextColor3 = ColorPalette.White,
             TextSize = 14,
             TextStrokeColor3 = ColorPalette.Black,
