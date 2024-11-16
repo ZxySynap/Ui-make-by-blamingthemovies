cf532af353973c617c31cd378f7efdafb08fca1f
ZxySynap <134072667+ZxySynap@users.noreply.github.com>
2024-11-16 23:55:18 +0700

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -799,7 +799,7 @@ function HorizonBarLibrary:CreateWindow(WindowProperties)
             Visible = true,
             ClipsDescendants = false,
             FontFace = Font.new("rbxasset://fonts/families/Ubuntu.json", Enum.FontWeight.Bold),
-            Text = os.date("%H:%M:%S") .. "น. (" .. game:GetService("LocalizationService").RobloxLocaleId .. ") © Zxysynap",
+            Text = os.date("%H:%M:%S") .. "น. (" .. game:GetService("LocalizationService").RobloxLocaleId .. ")",
             TextColor3 = ColorPalette.White,
             TextSize = 14,
             TextStrokeColor3 = ColorPalette.Black,
